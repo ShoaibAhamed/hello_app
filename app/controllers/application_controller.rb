e306bf480df307a3acd461755b370bb2e68677aa
shoaibnycda <shoaib@adrepublicbd.com>
2015-11-03 20:27:25 -0500

    2nd change

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,6 +4,6 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   
   def hello
-    render text: "Thank You James!"
+    render text: "Thank You Shoaib!"
   end
 end
